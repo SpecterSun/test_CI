c2b5234a49fd8bed99b16392c420bf9709350391
Alukard <137450843+SpecterSun@users.noreply.github.com>
2025-12-26 13:39:42 +0600

    Update test_triangle.py

--- test_triangle.py
+++ test_triangle.py
@@ -1,67 +1,50 @@
 import unittest
-from triangle import area, perimeter
+from square import area, perimeter
 
 
-class TriangleTestCase(unittest.TestCase):
+class SquareTestCase(unittest.TestCase):
 
     def test_area_normal(self):
-        self.assertEqual(area(10, 5), 25)
+        self.assertEqual(area(5), 25)
 
     def test_area_zero(self):
-        self.assertEqual(area(0, 5), 0)
-        self.assertEqual(area(10, 0), 0)
+        self.assertEqual(area(0), 0)
 
     def test_area_negative(self):
-        self.assertEqual(area(-10, 5), -25)
-        self.assertEqual(area(10, -5), -50)
+        self.assertEqual(area(-5), 25)
 
     def test_area_string(self):
-        self.assertEqual(area("10", 5), 50)
-        self.assertEqual(area(10, "5"), 50)
+        with self.assertRaises(TypeError):
+            area("5")
 
     def test_area_list(self):
-        self.assertEqual(area([10], 5), 50)
-        self.assertEqual(area(10, [5]), 50)
+        with self.assertRaises(TypeError):
+            area([5])
 
     def test_area_none(self):
-        self.assertEqual(area(None, 5), 0)
-        self.assertEqual(area(10, None), 0)
+        with self.assertRaises(TypeError):
+            area(None)
 
     def test_perimeter_normal(self):
-        self.assertEqual(perimeter(3, 4, 5), 12)
-
-    def test_perimeter_equal_sides(self):
-        self.assertEqual(perimeter(5, 5, 5), 15)
+        self.assertEqual(perimeter(5), 20)
 
     def test_perimeter_zero(self):
-        self.assertEqual(perimeter(0, 4, 5), 9)
-        self.assertEqual(perimeter(3, 0, 5), 8)
-        self.assertEqual(perimeter(3, 4, 0), 7)
+        self.assertEqual(perimeter(0), 0)
 
     def test_perimeter_negative(self):
-        self.assertEqual(perimeter(-3, 4, 5), 6)
-        self.assertEqual(perimeter(3, -4, 5), 4)
-        self.assertEqual(perimeter(3, 4, -5), 2)
-
-    def test_perimeter_triangle_inequality(self):
-        self.assertEqual(perimeter(1, 2, 10), 13)
-        self.assertEqual(perimeter(10, 2, 1), 13)
-        self.assertEqual(perimeter(2, 10, 1), 13)
+        self.assertEqual(perimeter(-3), -12)
 
     def test_perimeter_string(self):
-        self.assertEqual(perimeter("3", 4, 5), 12)
-        self.assertEqual(perimeter(3, "4", 5), 12)
-        self.assertEqual(perimeter(3, 4, "5"), 12)
+        with self.assertRaises(TypeError):
+            perimeter("10")
 
     def test_perimeter_list(self):
-        self.assertEqual(perimeter([3], 4, 5), 12)
-        self.assertEqual(perimeter(3, [4], 5), 12)
-        self.assertEqual(perimeter(3, 4, [5]), 12)
+        with self.assertRaises(TypeError):
+            perimeter([3])
 
     def test_perimeter_none(self):
-        self.assertEqual(perimeter(None, 4, 5), 9)
-        self.assertEqual(perimeter(3, None, 5), 8)
-        self.assertEqual(perimeter(3, 4, None), 7)
+        with self.assertRaises(TypeError):
+            perimeter(None)
 
 
 if __name__ == '__main__':
