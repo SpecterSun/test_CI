bba2bb6c55bbee7b98d4aca85ec2661d77a960fc
Alukard <137450843+SpecterSun@users.noreply.github.com>
2025-12-26 13:41:02 +0600

    Update test_triangle.py

--- test_triangle.py
+++ test_triangle.py
@@ -1,50 +1,59 @@
 import unittest
-from square import area, perimeter
+from triangle import area, perimeter
 
 
-class SquareTestCase(unittest.TestCase):
+class TriangleTestCase(unittest.TestCase):
 
     def test_area_normal(self):
-        self.assertEqual(area(5), 25)
+        self.assertEqual(area(10, 5), 25)
 
     def test_area_zero(self):
-        self.assertEqual(area(0), 0)
+        self.assertEqual(area(0, 5), 0)
+        self.assertEqual(area(10, 0), 0)
 
     def test_area_negative(self):
-        self.assertEqual(area(-5), 25)
+        self.assertEqual(area(-10, 5), -25)
+        self.assertEqual(area(10, -5), -25)
 
     def test_area_string(self):
         with self.assertRaises(TypeError):
-            area("5")
+            area("10", 5)
 
     def test_area_list(self):
         with self.assertRaises(TypeError):
-            area([5])
+            area([10], 5)
 
     def test_area_none(self):
         with self.assertRaises(TypeError):
-            area(None)
+            area(None, 5)
 
     def test_perimeter_normal(self):
-        self.assertEqual(perimeter(5), 20)
+        self.assertEqual(perimeter(3, 4, 5), 12)
+
+    def test_perimeter_equal_sides(self):
+        self.assertEqual(perimeter(5, 5, 5), 15)
 
     def test_perimeter_zero(self):
-        self.assertEqual(perimeter(0), 0)
+        self.assertEqual(perimeter(0, 4, 5), 9)
+        self.assertEqual(perimeter(3, 0, 5), 8)
+        self.assertEqual(perimeter(3, 4, 0), 7)
 
     def test_perimeter_negative(self):
-        self.assertEqual(perimeter(-3), -12)
+        self.assertEqual(perimeter(-3, 4, 5), 6)
+        self.assertEqual(perimeter(3, -4, 5), 4)
+        self.assertEqual(perimeter(3, 4, -5), 2)
 
     def test_perimeter_string(self):
         with self.assertRaises(TypeError):
-            perimeter("10")
+            perimeter("3", 4, 5)
 
     def test_perimeter_list(self):
         with self.assertRaises(TypeError):
-            perimeter([3])
+            perimeter([3], 4, 5)
 
     def test_perimeter_none(self):
         with self.assertRaises(TypeError):
-            perimeter(None)
+            perimeter(None, 4, 5)
 
 
 if __name__ == '__main__':
