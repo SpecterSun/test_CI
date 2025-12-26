f9ff72802599c2ca0030597a863d7c0cfd148360
Alukard <137450843+SpecterSun@users.noreply.github.com>
2025-12-26 13:46:40 +0600

    Update test_square.py

--- test_square.py
+++ test_square.py
@@ -14,14 +14,20 @@ class SquareTestCase(unittest.TestCase):
         self.assertEqual(area(-5), 25)
 
     def test_area_string(self):
-        self.assertEqual(area("5"), 25)
+        with self.assertRaises(TypeError):
+            area("5")
 
     def test_area_list(self):
-        self.assertEqual(area([5]), [5, 5, 5, 5, 5])
+        with self.assertRaises(TypeError):
+            area([5])
 
     def test_area_none(self):
         with self.assertRaises(TypeError):
             area(None)
+
+    def test_area_boolean(self):
+        self.assertEqual(area(True), 1)
+        self.assertEqual(area(False), 0)
 
     def test_perimeter_normal(self):
         self.assertEqual(perimeter(5), 20)
@@ -33,14 +39,20 @@ class SquareTestCase(unittest.TestCase):
         self.assertEqual(perimeter(-3), -12)
 
     def test_perimeter_string(self):
-        self.assertEqual(perimeter("10"), 40)
+        with self.assertRaises(TypeError):
+            perimeter("10")
 
     def test_perimeter_list(self):
-        self.assertEqual(perimeter([3]), [3, 3, 3, 3])
+        with self.assertRaises(TypeError):
+            perimeter([3])
 
     def test_perimeter_none(self):
         with self.assertRaises(TypeError):
             perimeter(None)
+
+    def test_perimeter_boolean(self):
+        self.assertEqual(perimeter(True), 4)
+        self.assertEqual(perimeter(False), 0)
 
 
 if __name__ == '__main__':
